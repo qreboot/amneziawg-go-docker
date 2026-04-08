fadf0dcc1cfdcd979251ac1f71992595ab067eae
eightbit <eightbit@eights.cloud>
2026-04-09 02:58:02 +0300

    EIGHT-0000 | fix: no qrcode

--- awgcfg.py
+++ awgcfg.py
@@ -8,7 +8,7 @@ import subprocess
 import optparse
 import random
 import datetime
-import qrcode
+
 from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey
 from cryptography.hazmat.primitives import serialization
 
